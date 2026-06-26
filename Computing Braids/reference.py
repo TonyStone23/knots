be4258cce87d1f2a07fb2ed1fba0416f02bf07b9
TonyStone23 <apstone45@gmail.com>
2026-06-26 12:55:18 -0400

    formatted tutorial

--- Computing Braids/reference.py
+++ Computing Braids/reference.py
@@ -2,17 +2,31 @@ from braid import main
 from webs import Braid, compose, power, build
 
 #===
+# Convention and Function Notes
+#---
+# The basis Elements are Braid.b(0-5) and correspond
+# to the order you have written them in notes.
+
+#---
+# compose(w2, w1) allows you to compose two braids:
+#
+#  | F | = compose(G, F)
+#  | G |  
+
+#---
+# power(braid, n) allows you to take the power of a braid
+
+#---
+# build([braid1, braid2, ..., braidn]) allows you to build a braid:
+#
+# compose(braid1, (compose (braid2, compose(..., braidn))))
+
+#===
 # Tutorial
 #---
 # main computes an actual braid from "Braid"
 print("\n\n--- Heres main ---")
 main(Braid.braid02, showinput=True)
-
-#---
-# Compose(w2, w1) allows you to compose two braids:
-#
-#  | F | = compose(G, F)
-#  | G |  
 
 #---
 # You can compute the composition of two braids.
@@ -33,12 +47,6 @@ print("\n\n--- heres a power example ---")
 braid = power(Braid.b5, 3)
 main(braid)
 
-#===
-# Additional Notes
-#---
-# The basis Elements are Braid.b(0-5) and correspond
-# to the order you have written them in notes.
-
 #---
 # You can build braids as a composition of basis elements, and take their power.
 print("\n\n--- Building a braid to take its power ---")
@@ -48,6 +56,7 @@ main(cubedBraid, showinput=True)
 
 #---
 # You can build a braid from a list of basis components:
-elements = [Braid.b3, Braid.b4, Braid.b2, Braid.b4, Braid.b2]
+print("\n\n--- Building a braid ---")
+elements = [Braid.b3, Braid.b4, Braid.b1, Braid.b4, Braid.b2]
 braid = build(elements)
 main(braid, showinput=True)
